a5601b2e97a05839b1567d74390cf49f295b604d
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-01 18:15:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -58,8 +58,7 @@ function displayCommits() {
 }
 function getBranches(el) {
   const repoName = el.dataset.repository;
-  const uri =
-    rootURL + '/repos/' + el.dataset.username + '/' + repoName + '/branches';
+  const uri = 'https://api.github.com/repos/' + el.dataset.username + '/' + repoName + '/branches';
   const xhr = new XMLHttpRequest();
   xhr.addEventListener('load', displayBranches);
   xhr.open('GET', uri);
