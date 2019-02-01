21bf9c2653e34d4c6a9bc5d7b6014f62fd94e530
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-01 18:13:12 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -12,7 +12,8 @@ function getRepositories() {
 
 function showRepositories(){
   var repos = JSON.parse(this.responseText); 
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>` ;
+  const repoList = `<ul>${repos.map(r => 
+    '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>` ;
   document.getElementById("repositories").innerHTML = repoList ;
 }
 
