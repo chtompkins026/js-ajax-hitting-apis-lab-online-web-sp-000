bc8f7874064f9311669b65cc7076d1c3d2e6b813
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-01 18:13:51 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -32,19 +32,46 @@ function showRepositories(){
 }
 
 
-function showCommits() {
-  const commits = JSON.parse(this.responseText) ;
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>` ;
-  document.getElementById("details").innerHTML = commitsList ;
-}
-
 function getCommits(el) {
-  const name = el.dataset.repo;
-  const username = el.dataset.repo;
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", showCommits);
-  req.open("GET", 'https://api.github.com/repos/' + user + '/' + name + '/commits'); 
-  req.send(); 
+  const repoName = el.dataset.repository;
+  const uri =
+    rootURL + '/repos/' + el.dataset.username + '/' + repoName + '/commits';
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayCommits);
+  xhr.open('GET', uri);
+  xhr.send();
+}
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><h3>' +
+        commit.commit.author.name +
+        ' (' +
+        commit.author.login +
+        ')</h3>' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
+function getBranches(el) {
+  const repoName = el.dataset.repository;
+  const uri =
+    rootURL + '/repos/' + el.dataset.username + '/' + repoName + '/branches';
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayBranches);
+  xhr.open('GET', uri);
+  xhr.send();
+}
+function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches
+    .map(branch => '<li>' + branch.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
 }
 
 
