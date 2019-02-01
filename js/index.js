02506461d708d37b89da1516ab55593f89bf9e67
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-01 17:54:02 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ function showRepositories(event, data) {
 
 function getRepositories() {
   const req = new XMLHttpRequest() ; 
-  const uri = rootUrl
+  const uri = 'https://api.github.com/' + 
   const user = document.getElementById('username').value ;
   req.addEventListener("load", showRepositories);
   req.open("GET", 'https://api.github.com/users/chtompkins026/repos') ; 
