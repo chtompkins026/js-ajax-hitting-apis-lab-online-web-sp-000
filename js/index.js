0aa3235b442f53fc691cfaaf09ddbce3d1786a48
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-01 17:40:28 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // your code here
+
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
@@ -8,7 +9,7 @@ function showRepositories(event, data) {
 function showCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("commits").innerHTML = commitsList
+  document.getElementById("details").innerHTML = commitsList
 }
 
 function getCommits(el) {
